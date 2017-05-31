53e85c8e4c20c217bdf4709070302376eb632be6
Michael Casadevall <mcasadevall@ubuntu.com>
2017-05-31 05:00:33 -0400

    Fix build_all script ndr_ota -> ndr_installer
    Signed-off-by: Michael Casadevall <mcasadevall@ubuntu.com>

--- build_all.sh
+++ build_all.sh
@@ -2,9 +2,9 @@
 make O=output_ndr_boot ndr_boot_defconfig
 make O=output_ndr_boot
 
-make O=output_ndr_ota ndr_ota_defconfig
-make O=output_ndr_ota
+make O=output_ndr_installer ndr_installer_defconfig
+make O=output_ndr_installer
 
 mkdir -p images
 cp output_ndr_boot/images/bzImage images/boot.efi
-cp output_ndr_ota/images/bzImage images/boot_ota.efi
+cp output_ndr_installer/images/bzImage images/boot_installer.efi
