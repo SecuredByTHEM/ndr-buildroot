f6bdf3ce26cefee5209c906891e6284b41f62a3d
Michael Casadevall <michaelc@them.com>
2017-09-27 22:44:46 -0400

    Commit force OTA code into github
    Signed-off-by: Michael Casadevall <michaelc@them.com>

--- build_all.sh
+++ build_all.sh
@@ -5,6 +5,11 @@ make O=output_ndr_boot
 make O=output_ndr_installer ndr_installer_defconfig
 make O=output_ndr_installer
 
+# We don't regularly build this image
+make O=output_ndr_force_ota ndr_force_ota_defconfig
+make O=output_ndr_force_ota
+
 mkdir -p images
 cp output_ndr_boot/images/bzImage images/boot.efi
 cp output_ndr_installer/images/bzImage images/boot_installer.efi
+cp output_ndr_force_ota/images/bzImage images/boot_force_ota.efi
